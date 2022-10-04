b4231d506ed22d7b0e28b89b35fa62b601f89a49
andrvale635 <89186098454valiok>
2022-10-05 00:07:09 +0300

    Hello world2

--- Prob1.java
+++ Prob1.java
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 public class Prob1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
-=======
-public class Prob1 {
-    public static void main(String[] args) {
 
-    }
-}
->>>>>>> origin/main
