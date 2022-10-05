01cf57d198888ad44cdd85a420735fa921391c96
andrvale635 <89186098454valiok>
2022-10-05 20:27:00 +0300

    squash! Hello world2

--- Prob1.java
+++ Prob1.java
@@ -4,6 +4,7 @@ public class Prob1 {
         System.out.println("new Hello");
         System.out.println("Stepan");
         System.out.println("sss");
+        System.out.println("fff");
     }
 }
 
